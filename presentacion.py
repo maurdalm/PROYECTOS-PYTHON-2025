60ba7cadad06fa772486919f2f5904cea48304a8
maurdalm <maurdalm@gmail.com>
2025-03-17 21:48:39 -0500

    Se agregan archivos

---
+++ presentacion.py
@@ -0,0 +1,4 @@
+#programa de presentacion#
+print("Nombre: Mauricio Daleman")
+print("Edad: 54 Años")
+print("Pais: Colombia")
---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variables en python
+
+edad = 54
+altura = 1.75
+pais = "Colombia"
+
+#acceder a las variables
+print("Valores iniciales: ")
+print("Edad:", edad)
+print("Altura:", altura)
+print("Pais:", pais)
+
+#Modificar el valor de una variable
+edad = 44
+altura = 1.73
+pais = "Colombia"
+
+#Valores modificados
+print("Valores Modificados: ")
+print("Edad:", edad)
+print("Altura:", altura)
+print("Pais:", pais)
