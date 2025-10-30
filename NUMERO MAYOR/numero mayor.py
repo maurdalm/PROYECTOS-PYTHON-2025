98cbb6d1f4f3ef9f21b0f6b6dbaa02d224691f83
maurdalm <maurdalm@gmail.com>
2025-10-29 20:39:52 -0500

    cambios_prog numero mayor 29/10/2025

--- NUMERO MAYOR/numero mayor.py
+++ NUMERO MAYOR/numero mayor.py
@@ -3,12 +3,12 @@ num1 = float(input("Ingresa el primer numero: "))
 num2 = float(input("Ingresa el segundo nmero: ")) 
 
 # Comparar los n~meros 
-if num1 >num2:
- print(f"Es numero mayor es: {num1}")
+if num1 > num2:
+    print(f"Es numero mayor es: {num1}")
 elif num1 < num2:
- print(f"El numero mayor es: {num2}") 
+    print(f"El numero mayor es: {num2}") 
 else:
- print("Ambos numeros son iguales") 
+    print("Ambos numeros son iguales") 
 
 # Opcion usando la funcion max()
- print(f"\n El Numero Mayor es: {max(num1, num2)}")
+    print(f"\n El Numero Mayor es: {max(num1, num2)}")
